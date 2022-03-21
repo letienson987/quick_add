d698ef11d3bd1c2d513f54848fccf8417c1d15b3
Lê Tiến Sơn <91727716+letienson987@users.noreply.github.com>
2022-03-21 23:51:09 +0700

    Ref #3

--- addFriend.js
+++ addFriend.js
@@ -1,10 +1,24 @@
+// is maintained
+const elm = $('.a8c37x1j.ni8dbmo4.stjgntxs.l9j0dhe7.ltmttdrg.g0qnabr5')
+const elm_array = Array.from($$('.a8c37x1j.ni8dbmo4.stjgntxs.l9j0dhe7.ltmttdrg.g0qnabr5'))
+let tmp = 1;
 
 
-const btn_add = $$('.a8c37x1j.ni8dbmo4.stjgntxs.l9j0dhe7.ltmttdrg.g0qnabr5')
+var time = setInterval(myTimer, 2000);
 
-function clickAdd() {
-    Array.from(btn_add).forEach(button => button.click())
-    
+
+function myTimer() {
+	if (tmp < elm_array.length) { 
+		elm.click();
+		// console.log('ok')
+		
+		tmp = tmp + 1;
+	} 
+	else {
+		console.log('done');
+		clearInterval(time);
+	}
+
 }
-
-setTimeout(clickAdd(), 10000);
+//  can use
+Array.from($$('.a8c37x1j.ni8dbmo4.stjgntxs.l9j0dhe7.ltmttdrg.g0qnabr5')).forEach(btn => btn.click())
